dd5dd3d45e47584b0a921415de53a9bc59294d56
Oliver vBK <olivervbk@gmail.com>
2017-01-10 23:43:16 -0200

    Base structure in parser

--- lib/moodle_gift/parser.rb
+++ lib/moodle_gift/parser.rb
@@ -1,7 +1,16 @@
 module MoodleGift
   class Parser
     def parse(content)
-      
+      questions = []
+
+      category = nil
+      comment = nil
+      question = nil
+      content.each_line do |line|
+
+      end
+
+      return questions
     end
   end #class
 end #module
